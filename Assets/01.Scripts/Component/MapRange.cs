86e1c6b0a631430d9e909a0e8ef7a36ebec1f3d5
ekflwk9 <ekflwk9@naver.com>
2025-04-30 01:19:52 +0900

    [feat] MapRange 스크립트 추가
    맵 범위에 관한 스크립트 추가

--- Assets/01.Scripts/Component/MapRange.cs
+++ Assets/01.Scripts/Component/MapRange.cs
@@ -1,6 +1,19 @@
 using UnityEngine;
 
-public class MapRange : MonoBehaviour
+public class MapRange : MonoBehaviour,
+IAwake
 {
+    [Header("¸Ê ¹üÀ§")]
+    [SerializeField] private Vector2 range;
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(this.transform.position, range);
+    }
+
+    public void OnAwake()
+    {
+        GameManager.SetComponent(this);
+    }
 }
